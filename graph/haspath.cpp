5861573c9a67e4abafdbbd00b629df6950517ac4
Debmalya Das <debmalya017@gmail.com>
2024-08-03 00:35:02 +0530

    haspath

---
+++ graph/haspath.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+// dfs
+// bool haspath(unordered_map<char, vector<char>> g, char s, char d)
+// {
+//     if (s == d)
+//         return 1;
+//     for (auto i : g)
+//     {
+//         if (i.first == s)
+//         {
+//             for (auto j : i.second)
+//             {
+//                 if (haspath(g, j, d) == 1)
+//                 {
+//                     return 1;
+//                 }
+//             }
+//         }
+//     }
+//     return 0;
+// }
+
+// bfs
+bool haspath(unordered_map<char, vector<char>> g, char s, char d)
+{
+
+    queue<char> q;
+    q.push(s);
+    while (q.size() > 0)
+    {
+        char curr = q.front();
+        q.pop();
+        if (s == d)
+            return 1;
+        for (auto i : g)
+        {
+            if (i.first == s)
+            {
+                for (auto j : i.second)
+                {
+                    if (haspath(g, j, d) == 1)
+                    {
+                        return 1;
+                    }
+                }
+            }
+        }
+    }
+
+    return 0;
+}
+int main()
+{
+    unordered_map<char, vector<char>> g;
+    g['a'].push_back('b');
+    g['a'].push_back('c');
+    g['b'].push_back('d');
+    g['c'].push_back('e');
+    g['d'].push_back('f');
+
+    cout << haspath(g, 'c', 'f');
+}
